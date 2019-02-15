23620c1b14fb982a29fa450a42279b3cc0ccbaf1
liuxueting121 <2097774160@qq.com>
2019-02-15 21:27:40 +0800

    登录-路由配置

---
+++ src/components/login.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="login-wrap">
+        <el-form label-position="top" label-width="80px" :model="formData" class="login-form">
+            <h2>用户登录</h2>
+            <el-form-item label="用户名">
+                <el-input v-model="formData.name"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="formData.password"></el-input>
+            </el-form-item>
+            <el-button type="primary" class="login-button">登录</el-button>
+        </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        username: "",
+        password: ""
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.login-wrap {
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+.login-form {
+    background-color: #fff;
+    padding: 30px;
+    border-radius: 10px;
+    width: 400px;
+}
+.login-wrap,.login-button {
+    width: 100%;
+}
+</style>
