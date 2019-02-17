a9815eb8e80c5210c43d9d91a505ea678281808f
liuxueting121 <2097774160@qq.com>
2019-02-17 23:53:41 +0800

    用户列表-请求数据

--- src/components/users.vue
+++ src/components/users.vue
@@ -13,49 +13,56 @@
         <!-- 搜索框 -->
         <el-row>
             <el-col>
-                <el-input placeholder="请输入内容" v-model="input5" class="searchbox">
+                <el-input placeholder="请输入内容"  class="searchbox">
                     <el-button slot="append" icon="el-icon-search"></el-button>
                 </el-input>
                 <el-button type="primary">添加用户</el-button>
-
-                <!-- 表格 -->
-                <el-table :data="tableData" style="width: 100%">
-                    <el-table-column prop="date" label="日期" width="180">
-                    </el-table-column>
-                    <el-table-column prop="name" label="姓名" width="180">
-                    </el-table-column>
-                    <el-table-column prop="address" label="地址">
-                    </el-table-column>
-                </el-table>
             </el-col>
         </el-row>
+
+        <!-- 表格 -->
+        <el-table :data="list" style="width: 100%">
+            <el-table-column prop="name" label="#" width="80"></el-table-column>
+            <el-table-column prop="name" label="姓名" width="100"></el-table-column>
+            <el-table-column prop="name" label="邮箱" width="140"></el-table-column>
+            <el-table-column prop="name" label="电话" width="140"></el-table-column>
+            <el-table-column prop="name" label="创建日期" width="200"></el-table-column>
+            <el-table-column prop="name" label="用户状态" width="120"></el-table-column>
+            <el-table-column prop="name" label="操作" width="200"></el-table-column>
+        </el-table>
+
     </el-card>
 </template>
 <script>
 export default {
-      data() {
-        return {
-            query:"",
-            //表格数据
-          tableData: [{
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-          }, {
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄'
-          }, {
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
-          }, {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          }]
-        }
+  data() {
+    return {
+        // 写搜索功能时的关键字
+      query: "",
+      //分页功能-前提是接口必须支持分页-通常在接口url参数中有类似page的参数名
+      pagenum:1,
+      pagesize:6,
+      //表格数据
+      list:[]
+    };
+  },
+  created(){
+      this.getTableData()
+  },
+  methods: {
+     async getTableData(){
+        //  设置请求头
+        // 设置发送请求时的请求头->axios库->找axios中有没有可以设置headers头部的API
+        const AUTH_TOKEN = localStorage.getItem('token');
+        console.log(AUTH_TOKEN);
+        
+        this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+          const res = await this.$http.get(`users?query=${this.query}&pagenum=${this.pagrnum}&pagesize=${this.pagesize}`)
+          console.log(res);
+          
       }
+      
+  }
 };
 </script>
 
