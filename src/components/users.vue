6d8b32781e1d630410358f4e2395a90b046da512
liuxueting121 <2097774160@qq.com>
2019-02-17 20:26:50 +0800

    用户管理-用户列表-面包屑or搜索框

--- src/components/users.vue
+++ src/components/users.vue
@@ -1,9 +1,24 @@
 <template>
-    <div>
-        users----
-    </div>
-</template>
+    <!-- 面包屑 -->
+    <el-card class="box">
+        <el-breadcrumb separator="/">
+  <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+  <el-breadcrumb-item><a href="/">用户管理</a></el-breadcrumb-item>
+  <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+ 
+</el-breadcrumb>
 
+<!-- 搜索框 -->
+ <el-row>
+     <el-col>
+          <el-input placeholder="请输入内容" v-model="input5" class="searchbox">
+              <el-button slot="append" icon="el-icon-search"></el-button>
+              </el-input>
+              <el-button type="primary">添加用户</el-button>
+     </el-col>
+ </el-row>
+    </el-card>
+</template>
 <script>
 export default {
 
@@ -11,5 +26,8 @@ export default {
 </script>
 
 <style>
-
+    .searchbox {
+        margin-top: 20px;
+        width: 400px;
+    }
 </style>
