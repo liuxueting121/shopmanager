9d6a3fe631eb93193d9c237c0b8442752971537d
liuxueting121 <2097774160@qq.com>
2019-02-21 00:15:15 +0800

    权限列表-自定义面包屑组件

--- src/components/rights.vue
+++ src/components/rights.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>rights--</div>
+    <cus-bread level1="权限管理" level2="权限列表"></cus-bread>
 </template>
 
 <script>
@@ -9,5 +9,7 @@ export default {
 </script>
 
 <style>
-
+    .box {
+        height: 100%;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import CusBread from './components/cusBread.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
@@ -18,6 +19,8 @@ Vue.config.productionTip = false
 Vue.filter('fmtdate',(v) => {
   return moment(v).format('YYYY-MM-DD')
 });
+// 全局自定义组件
+Vue.component(CusBread.name,CusBread);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
