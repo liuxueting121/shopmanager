ba18c612713f42dfcf1fcf776bcc424d4369d897
liuxueting121 <2097774160@qq.com>
2019-02-22 21:20:27 +0800

    首页-侧边栏-动态导航

--- src/components/home.vue
+++ src/components/home.vue
@@ -18,70 +18,16 @@
             <el-aside class="aside" width="200px">
                 <el-menu default-active="1" :router="true" :unique-opened="true">
                     <!-- 用户管理 -->
-                    <el-submenu index="1">
+                    <el-submenu :index="item1.order+''"
+                    v-for="(item1,i) in menus" :key="item1.id"
+                    >
                         <template slot="title">
                             <i class="el-icon-location"></i>
-                            <span>用户管理</span>
+                            <span>{{item1.authName}}</span>
                         </template>
-                        <el-menu-item index="users">
+                        <el-menu-item :index="item2.path+''" v-for="(item2,i) in item1.children" :key="item2.id">
                             <i class="el-icon-menu"></i>
-                            用户列表
-                        </el-menu-item>
-                    </el-submenu>
-                    <!-- 权限管理 -->
-                    <el-submenu index="2">
-                        <template slot="title">
-                            <i class="el-icon-location"></i>
-                            <span>权限管理</span>
-                        </template>
-                        <el-menu-item index="roles">
-                            <i class="el-icon-menu"></i>
-                            角色列表
-                        </el-menu-item>
-                        <el-menu-item index="rights">
-                            <i class="el-icon-menu"></i>
-                            权限列表
-                        </el-menu-item>
-                    </el-submenu>
-                    <!-- 商品 -->
-                    <el-submenu index="3">
-                        <template slot="title">
-                            <i class="el-icon-location"></i>
-                            <span>商品管理</span>
-                        </template>
-                        <el-menu-item index="1-1">
-                            <i class="el-icon-menu"></i>
-                            商品列表
-                        </el-menu-item>
-                        <el-menu-item index="1-1">
-                            <i class="el-icon-menu"></i>
-                            分类参数
-                        </el-menu-item>
-                        <el-menu-item index="1-1">
-                            <i class="el-icon-menu"></i>
-                            商品分类
-                        </el-menu-item>
-                    </el-submenu>
-                    <!-- 订单 -->
-                    <el-submenu index="4">
-                        <template slot="title">
-                            <i class="el-icon-location"></i>
-                            <span>订单管理</span>
-                        </template>
-                        <el-menu-item index="1-1">
-                            <i class="el-icon-menu"></i>
-                            订单列表
-                        </el-menu-item>
-                    </el-submenu>
-                    <!-- 数据 -->
-                    <el-submenu index="5">
-                        <template slot="title">
-                            <i class="el-icon-location"></i>
-                            <span>数据统计</span>
-                        </template>
-                        <el-menu-item index="1-1">
-                            <i class="el-icon-menu"></i>
-                            数据列表
+                            {{item2.authName}}
                         </el-menu-item>
                     </el-submenu>
                 </el-menu>
@@ -96,8 +42,13 @@
 
 <script>
 export default {
+    data(){
+        return{
+            menus:[]
+        }
+    },
   beforeMount () {
-    console.log('beforeMount---')
+    // console.log('beforeMount---')
 
     if (!localStorage.getItem('token')) {
       this.$router.push({
@@ -105,7 +56,21 @@ export default {
       })
     }
   },
+  created() {
+      this.getMenus();
+  },
   methods: {
+      async getMenus() {
+          const res = await this.$http.get(`menus`);
+          console.log(res);
+          const {meta:{msg,status},data} = res.data;
+          if (status===200) {
+              this.menus = data;
+              console.log(this.menus);
+              
+          } 
+      },
+    //   退出
     handleloginout () {
       // 1.清除token
       localStorage.clear()
